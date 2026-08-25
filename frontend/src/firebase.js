7d6172264818287c57b20626da33209a32e3e0dc
USER <USER@DESKTOP-CCLAF6D>
2026-08-25 15:35:15 +0530

    Update firebase.js to include databaseURL

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,6 +5,7 @@ import { getFirestore } from 'firebase/firestore'
 const config = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyBPOeXPgVJ4ukUCg7tPU8Iy6nkK2EMGeYs",
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "subscription-free-681aa.firebaseapp.com",
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL || "https://subscription-free-681aa-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "subscription-free-681aa",
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "subscription-free-681aa.firebasestorage.app",
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "1022409104304",
